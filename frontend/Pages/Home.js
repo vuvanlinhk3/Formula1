043583d5ea10021128fbc77d55a0241e4744637a
Livaan <vanlinh30122003@gmail.com>
2024-09-30 19:18:15 +0700

    xong navbar

--- frontend/Pages/Home.js
+++ frontend/Pages/Home.js
@@ -1,9 +1,10 @@
 import { renderHeader } from '../components/Header.js';
 import { renderFooter } from '../components/Footer.js';
+import { renderNavBar } from '../components/NavBar.js';
 
 export function renderHomePage(root) {
   renderHeader(root);
-
+  renderNavBar(root);
   const homeContent = document.createElement('div');
   homeContent.innerHTML = `
     <h1>Welcome to the Home Page</h1>
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,20 +1,20 @@
 export function renderHeader(root) {
-    const header = document.createElement('header');
-    header.innerHTML = `
-      <nav>
-        <a href="#" id="navHome">Home</a>
-        <a href="#" id="navAbout">About</a>
-        <a href="#" id="navContact">Contact</a>
-      </nav>
-    `;
-    root.appendChild(header);
+    // const header = document.createElement('header');
+    // header.innerHTML = `
+    //   <nav>
+    //     <a href="#" id="navHome">Home</a>
+    //     <a href="#" id="navAbout">About</a>
+    //     <a href="#" id="navContact">Contact</a>
+    //   </nav>
+    // `;
+    // root.appendChild(header);
   
-    // Sự kiện điều hướng qua các link trong header
-    document.getElementById('navHome').addEventListener('click', function () {
-      navigateTo('home');
-    });
-    document.getElementById('navAbout').addEventListener('click', function () {
-      navigateTo('about');
-    });
+    // // Sự kiện điều hướng qua các link trong header
+    // document.getElementById('navHome').addEventListener('click', function () {
+    //   navigateTo('home');
+    // });
+    // document.getElementById('navAbout').addEventListener('click', function () {
+    //   navigateTo('about');
+    // });
   }
   
